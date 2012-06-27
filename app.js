dc248f4b02cfc8fbc22611d14a4478191cb3229c
Kazuyuki Honda <hakobera@gmail.com>
2012-06-28 00:22:47 +0900

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('hello');
+});
+server.listen(3000, function () {
+  console.log('Server is running on port %d', server.address().port);
+});
