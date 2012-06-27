c9f10d452817a622a8eece95be3e65aa2aa86807
Kazuyuki Honda <hakobera@gmail.com>
2012-06-28 04:25:20 +0900

    Change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('hello');
+  res.end('hello tng6');
 });
 server.listen(3000, function () {
   console.log('Server is running on port %d', server.address().port);
